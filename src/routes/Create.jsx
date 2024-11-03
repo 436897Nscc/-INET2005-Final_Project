f57ad3853f0c8b4552852eee4abbaeef8bc55624
MattG-M <w0436897@nscc.ca>
2024-11-03 18:24:52 -0400

    Unable to figue out why I'm not sending the requared fields. At least got the framework together

--- src/routes/Create.jsx
+++ src/routes/Create.jsx
@@ -5,32 +5,49 @@ import { useFormData } from 'herotofu-react';
 export default function Create(){
 
 
+    // url's used 
+    // https://www.react-hook-form.com/api/useform/handlesubmit/
+    // https://stackoverflow.com/questions/60901667/how-do-i-perform-a-post-request-only-when-form-is-validated-with-react-hook-form
+    // https://react-hook-form.com/docs/useform/handlesubmit
+    // https://react-hook-form.com/docs/useform/formstate
+    // https://stackoverflow.com/questions/68505071/how-do-i-post-form-data-using-fetch-and-react   
     
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    import.meta.env.VITE_API_HOST;
-    const [bio, setBio] = useState(null);
-
-    useEffect(() => {
-      // Fetch data from API
-      
-      async function fetchData() {
+    const { register, handleSubmit, formState: { errors, isSubmitting, touchedFields, submitCount } } = useForm();
+    async function fetchData() {
         const response = await fetch('http://localhost:3000/api/contacts/create'); 
         const data = await response.json();
         if (!ignore) {
           setBio(data);
         }
-      }
+      
   
       let ignore = false;
         fetchData();
       return () => {
          ignore = true;
       }
-    }, []);
-    const { formState, getFormSubmitHandler } = useFormData('http://localhost:3000/api/contacts/create');
+    }
+    const onSubmit = async (e) => {
+        console.log(JSON.stringify(e));
+        console.log('Submitting form');
+        const body = JSON.parse(JSON.stringify(e));
+        fetch('http://localhost:3000/api/contacts/create', {
+            mode:  'no-cors',
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+
+          body: JSON.stringify(body)
+        }).then((response) => {
+            console.log(response);
+            return response.json(); // do something with response JSON
+          });
+        };
+    const onError = (errors, e) => console.log(errors, e);
     return(
         <>
-            {!!formState.status && <div>Current form status is: {formState.status}</div>}
+           
             
             <h1>Create new contact</h1>
             <header>
@@ -47,21 +64,21 @@ export default function Create(){
                     <Link to="/delete">Delete</Link>
                 </p>
             </header>
-            <form id='newContact' onSubmit={getFormSubmitHandler()}>
+            <form id='newContact' onSubmit={handleSubmit(onSubmit)}>
                 <label className="form-label">First Name </label>
-                <input {...register("name")} type="text" className="form-control bg-light" />
+                <input {...register("firstName")} type="text" className="form-control bg-light" />
                 <br></br>
                 <label className="form-label">Last Name </label>       
-                <input {...register("last name")} type="text" className="form-control bg-light" />
+                <input {...register("lastName")} type="text" className="form-control bg-light" />
                 <br></br>   
                 <label className="form-label">Title </label>
                 <input {...register("title")} type="text" className="form-control bg-light" />
                 <br></br>    
-                <label className="form-label">Email </label>
-                <input {...register("email")} type="text" className="form-control bg-light" />
-                <br></br>   
                 <label className="form-label">Phone </label>
                 <input {...register("phone")} type="text" className="form-control bg-light" />
+                <br></br>   
+                <label className="form-label">Email </label>
+                <input {...register("email")} type="text" className="form-control bg-light" />
                 <br></br>
                 <label className="form-label">Picture</label>
                 <input {...register("filename")} type="text" className="form-control bg-light" />
